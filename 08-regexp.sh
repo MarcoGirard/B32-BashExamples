69b80fd0f52072024aa9f1292854732cc582909e
Marc-Olivier Girard <marc.olivier.girard.3@gmail.com>
2017-04-26 15:11:06 -0400

    expressions regulieres

--- 08-regexp.sh
+++ 08-regexp.sh
@@ -1,2 +1,15 @@
 #!/bin/bash
 
+# Identique à read -p "Lettre: " lettre
+echo -n "Lettre: "
+read lettre
+
+# ~ = regexp!
+if [[ $lettre =~ ^[0-9]+$ ]]
+then 
+	echo "C'est un nombre"
+fi
+if [[ $lettre =~ ^[a-zA-Z\s]+$ ]]
+then
+	echo "C'est un mot"
+fi
